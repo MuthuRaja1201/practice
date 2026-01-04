0e82f8123e79ed94123559abc2fb456d32677980
MuthuRaja1201 <160619076+MuthuRaja1201@users.noreply.github.com>
2026-01-04 12:03:34 +0000

    Message("palindrome number")

--- palindromenumber.java
+++ palindromenumber.java
@@ -3,6 +3,19 @@ public class palindromenumber{
     public static void main(String [] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+        int t=n;
+        int rev=0;
+        while(n>0){
+            int digit=n%10;
+            rev=rev*10+digit;
+            n=n/10;
+        }
+        if(t==rev){
+            System.out.println("palindrome");
+        }
+        else{
+            System.out.println("not palindrome");
+        }
         
     }
 }
