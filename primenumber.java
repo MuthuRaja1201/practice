f7fb6b49a9583a844c666e7af985d04bb7d8b7e8
MuthuRaja1201 <160619076+MuthuRaja1201@users.noreply.github.com>
2026-01-04 12:42:25 +0000

    Message("prime number check")

--- primenumber.java
+++ primenumber.java
@@ -7,7 +7,7 @@ public class primenumber {
             System.out.print("Not prime");
             return;
         }
-        for(int i=2;i<=n/2;i++){
+        for(int i=2;i*i<=n;i++){
             if(n%i==0){
                 System.out.print("Not prime");
                 return;
